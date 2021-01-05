a61f497150c8961e3757d18ee6aade75c39f186b
Johnny Noble <github-a3bd@johnny.net.au>
2021-01-06 10:41:34 +1100

    Create tailwind config (manually)
    https://tailwindcss.com/docs/installation#3-create-your-tailwind-config-file-optional

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+// https://tailwindcss.com/docs/installation#3-create-your-tailwind-config-file-optional
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
