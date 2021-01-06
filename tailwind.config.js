44308af6cd1ded3bc9bce7e8ec3eb651f911c2ba
Johnny Noble <github-a3bd@johnny.net.au>
2021-01-06 11:37:19 +1100

    fontFamily: Avenir Next

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      astronological: ['"Avenir Next"'],
+    },
     extend: {},
   },
   variants: {},
