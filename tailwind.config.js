508a87ed71502134f9dd01afd9ec26ddbd4d4b39
Johnny Noble <github-a3bd@johnny.net.au>
2021-04-19 15:42:49 +1000

    CSS: add custom CSS for links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 // tailwind.config.js
 // https://tailwindcss.com/docs/installation#3-create-your-tailwind-config-file-optional
 module.exports = {
-  purge: ["./*.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      astronological: ['"Avenir Next"'],
-    },
-    container: {
-      center: true,
-    },
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+	purge: ["./**/*.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			astronological: ['"Avenir Next"'],
+		},
+		container: {
+			center: true,
+		},
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
 };
